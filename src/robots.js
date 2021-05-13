82720d52803190725c3f70649bf942b8ec46a777
Lalith18 <lalithleftie@gmail.com>
2021-05-13 10:48:42 +0530

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'lalith',
+    email: 'lalith@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'harish',
+    email: 'harish@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'ram',
+    email: 'ram@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'kc',
+    email: 'kc@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'azim',
+    email: 'azim@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'eshwar',
+    email: 'eshwar@gmail.com',
+  },
+  {
+    id: 7,
+    name: 'kishore',
+    email: 'kishore@gmail.com',
+  },
+  
+]
